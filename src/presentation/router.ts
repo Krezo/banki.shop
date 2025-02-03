5d1f2ac662dd99c60b986cf21ba12176582e64c9
antonbezushko <bezushko.aiu@gmail.com>
2025-02-03 08:07:36 +0500

    Switch /shop to / path

--- src/presentation/router.ts
+++ src/presentation/router.ts
@@ -1,14 +1,13 @@
 import { RouterOptions } from 'vue-router';
 import LayoutDefault from '@/presentation/layouts/LayoutDefault.vue';
 import { shopRoutes } from './router/shop';
-import { homeRoutes } from './router/home';
 
 export const routerConfig: RouterOptions = {
   routes: [
     {
       path: '/',
       component: LayoutDefault,
-      children: [...shopRoutes, ...homeRoutes],
+      children: [...shopRoutes],
     },
   ],
   mode: 'history',
